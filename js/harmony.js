72700ac1fad499b0cb5c4cdc2fbebcce5979680a
Houssem88 <Houssem88@github.com>
2017-04-11 18:42:22 +0200

    reading zip, or skinpacks, and i can look inside

--- js/harmony.js
+++ js/harmony.js
@@ -1,8 +1,15 @@
-function zipIt(){
+function getSkinPack(){
 	var zip = new JSZip();
-	
+  	zip.loadAsync( document.getElementById("skin-rep").files[0] /* = file blob */)
+    .then(function(zip) {
+         // process ZIP file content here
+        console.log(zip);
+     }, function() {alert("Not a valid zip file")});
+}
 
-	zip.file("file.txt", "content");
-
-	console.log(zip);
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+} else {
+  alert('The File APIs are not fully supported in this browser.');
 }
