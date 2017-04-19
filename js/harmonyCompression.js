71cea1141b3a72ee05cca2122378516933362164
Houssem88 <Houssem88@github.com>
2017-04-19 10:00:49 +0200

    Now we can see the content

--- js/harmonyCompression.js
+++ js/harmonyCompression.js
@@ -5,14 +5,23 @@ function getSkinPack(){
     .then(function(zip) {
          // process ZIP file content here
 
-        /*NEED TO FIND A WAY TO GET THE CSS FILE INSIDE THE ZIP !!!!! */
-        var insideZIP = zip.files["example/css/stuff.css"]._data;
-        console.log(insideZIP);
+        /*NEED TO FIND A WAY TO GET THE CSS FILE INSIDE THE ZIP !!!!! BECAUSE NOW, IT'S HARD CODED */
+        var insideZIP = zip.files["example/css/stuff.css"]; //<<<<<< i'm talking about this 
+        /*console.log(insideZIP);*/
         document.getElementById("successZip").innerHTML = "skinpack chargé, et prêt à être modifié";
-    }, function() {
-  	
-    	document.getElementById("errorZip").innerHTML = "Veuillez importer un dossier .skinpack";
-    });
+
+  //NEXT is just to "see" the content of the css file
+  zip.file(insideZIP.name).async("string").then(function success(content) {
+    // use the content
+    console.log(content);
+  }, function error(e) {
+    // handle the error
+    console.log("erreur");
+  });
+
+  }, function() {
+    document.getElementById("errorZip").innerHTML = "Veuillez importer un dossier .skinpack";
+  });
 }
 
 // Check for the various File API support.
@@ -20,4 +29,8 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
   // Great success! All the File APIs are supported.
 } else {
   alert('The File APIs are not fully supported in this browser.');
+}
+
+function changeColorsText(){
+
 }
