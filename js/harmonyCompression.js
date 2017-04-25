4b707a8a5a449a6f7779c2a0fd014c37b2c09664
Houssem88 <Houssem88@github.com>
2017-04-25 17:56:00 +0200

    previsualisation dynamic color changes

--- js/harmonyCompression.js
+++ js/harmonyCompression.js
@@ -1,7 +1,7 @@
-function getSkinPack(){
-	var zip = new JSZip();
 
-  	zip.loadAsync( document.getElementById("skin-rep").files[0] /* = file blob */)
+function getSkinPack(){
+	var zip = new JSZip();  
+  zip.loadAsync( document.getElementById("skin-rep").files[0] /* = file blob */)
     .then(function(zip) {
          // process ZIP file content here
 
@@ -25,7 +25,11 @@ function getSkinPack(){
 
   }, function(){
     $("#msgSkinpackImportation").empty().append("Erreur lors du chargement").css("color", "red");
-  });
+  }); 
+}
+
+function validateStyle(lastName){
+
 }
 
 // Check for the various File API support.
@@ -33,8 +37,4 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
   // Great success! All the File APIs are supported.
 } else {
   alert('The File APIs are not fully supported in this browser.');
-}
-
-function changeColorsText(){
-
 }
