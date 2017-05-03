e7769b877b4157b284cfdbc6ecf649018836459e
Houssem88 <Houssem88@github.com>
2017-05-03 13:24:06 +0200

    cleaning code

--- js/harmonyZip.js
+++ js/harmonyZip.js
@@ -3,13 +3,20 @@ function getSkinPack(){
 	var zip = new JSZip();  
   zip.loadAsync( document.getElementById("skin-rep").files[0])
   .then(function(zip){
-    var insideZIP = zip.files["example/css/stuff.css"];
+    /*var insideZIP = zip.files["example/css/stuff.css"];*/
+    var allSkin = zip.files;
+
+    localStorage.setItem("allSkin", allSkin); // "allskin" contains all skins files.
+
+    var insideZIP = zip.files["skin/Jm71SrwTAbhQaExrdSmV3d.doss/css/skin.css"]; // bad way, need to change it for a fetch
+    
     $("#msgSkinpackImportation").empty().append("Skinpack chargé, et prêt à être modifié").css("color", "#2E7D32");
  
     //NEXT is just to "see" the content of the css file
     zip.file(insideZIP.name).async("string").then(function success(content) {
       // use the content
-      console.log(content);
+      /*console.log(content);*/
+      localStorage.setItem("skinCSS", content);
     },
     function error(e) {
       // handle the error
@@ -19,9 +26,6 @@ function getSkinPack(){
 
   function(){
     $("#msgSkinpackImportation").empty().append("Erreur lors du chargement").css("color", "red");
-  },
-  function(){
-    
   });
 }
 
@@ -33,5 +37,23 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 }
 
 function validateStyle(){
+  var updateSkinCss = localStorage.getItem("skinCSS");
+  console.log("from local"+updateSkinCss);
+}
 
+function test(){
+  var zip = new JSZip();
+
+  var getAllSkinFiles = localStorage.getItem("allSkin"); console.log(getAllSkinFiles);
+  /*var getSkinCss = localStorage.getItem("skinCSS");*/
+
+ /* zip.file("skin.css", getSkinCss);*/
+ /* zip.folder(getAllSkinFiles).file("skin.css", getSkinCss);*/
+
+  zip.folder(getAllSkinFiles);
+  zip.generateAsync({type:"blob"})
+  .then(function(content) {
+    // see FileSaver.js
+    saveAs(content, "example.zip");
+  });
 }
