1fe0c3d5137947dae3fb1c05312d64a15dc85cb6
Houssem88 <Houssem88@github.com>
2017-06-07 09:25:43 +0200

    writing on skin and main

--- js/harmonyZip.js
+++ js/harmonyZip.js
@@ -1,3 +1,17 @@
+var maLib = {
+  skinFiles : {
+    "main.css":content,
+  },
+  onLoad : function(pEvent){
+    console.log("aaa");
+  },
+  loadSkinPackValues : function(pEvent){
+    console.log("b");
+  }
+};
+
+/*window.addEventListener("load", maLib.onLoad, false);*/
+/*console.log(maLib);*/
 
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
@@ -13,70 +27,22 @@ var zip = new JSZip();
 
 /**********  Variables de l'UI   *********************/
 var content;
-var getPrincipalColor;
+var getPrincipalColor = '';
 
 document.getElementById('hrColorPrincipale').addEventListener('change', function(getPrincipalColor){
   getPrincipalColor = this.value;
   console.log(getPrincipalColor); //Je veux retourner getPrincipalColr et l'utiliser à la ligne 41
-  return getPrincipalColor;
 }, false);
 
 /******************************************************/
 
 /*  Validation du style et génération du nouveau skinpack */
 /*$(".validate-bloc").show();*/
-JSZipUtils.getBinaryContent('starter/harmonySRC0-1_2.zip', function(err, data) {
-  
-  if(err) {
-    throw err;
-  }
-
-  zip.loadAsync(data).then(function (zip){
-    var skinpack = zip.files;
-    return skinpack;
-  })
-  .then(function(skinpack){
-    //Get skin.css
-    var contentSkinCss = skinpack["skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/skin.css"];
-    console.log("skin.css : ", contentSkinCss);
-    zip.file(contentSkinCss.name).async("string").then(function success(content) {
-      // Get the color from UI
-      /*var principalColor = "\n.header{ background-color:#"+$("#hrColorPrincipale").val()+";}\n";*/
-      var x = getChangeColor1();
-      zip.file("skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/skin.css", content+x);
-      return (content);
-    });
-    return(skinpack)
-  })
-  .then(function(skinpack){
-    //Get main.css
-    var contentMainCss = skinpack["skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/main.css"];
-    console.log("main.css : ", contentMainCss);
-
-    zip.file(contentMainCss.name).async("string").then(function success(content) {
-      zip.file("skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/main.css", content+"modify main.css");
-        return (content)
-    });
-    return(skinpack);
-  })
-  .then(function(skinpack){
-    /*get skinSet.xml*/
-    var contentSkinXml = skinpack["skinSet.xml"];
-    console.log("skinSet.xml : ", contentSkinXml)
-
-    zip.file(contentSkinXml.name).async("string").then(function success(contentXML){
-
-    });
-  });
-});
-
-function getChangeColor1(){
-  var getPrincipalColor = document.getElementById("hrColorPrincipale").value;
-  return (getPrincipalColor);
-}
 
 
-    /*******************************************************************************************/
+
+
+/*******************************************************************************************/
 /*    zip.file(skinsetXML.name).async("string").then(function success(contentXML){
       parsSkinXML = new DOMParser();
       xmlDoc = parsSkinXML.parseFromString(contentXML,"text/xml");
@@ -130,8 +96,65 @@ function modifyXML(){
 
 
 //  Compresser et telecharger le skinpack modifié
-function validateStyle(content){
-  zip.generateAsync({type:"blob"}).then(function(content) {
-    saveAs(content, "skinpack.zip");
+function validateStyle(){
+  var zip = new JSZip();
+  JSZipUtils.getBinaryContent('starter/harmonySRC0-1_2.zip', function(err, data) {
+  
+    if(err) {
+      throw err;
+    }
+
+    zip.loadAsync(data).then(function (zip){
+      var skinpack = zip.files;
+      return skinpack;
+    })
+    .then(function(skinpack){
+      //Get skin.css
+      var contentSkinCss = skinpack["skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/skin.css"];
+      console.log("skin.css : ", contentSkinCss);
+      zip.file(contentSkinCss.name).async("string").then(function success(content){
+        // Get the color from UI
+        /*var principalColor = "\n.header{ background-color:#"+$("#hrColorPrincipale").val()+";}\n";*/
+        var x = document.getElementById('hrColorPrincipale').value;
+        zip.file("skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/skin.css", content+x);
+        return (content);
+      });
+      return(skinpack);
+    })
+    .then(function(skinpack){
+      //Get main.css
+      var contentMainCss = skinpack["skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/main.css"];
+      console.log("main.css : ", contentMainCss);
+
+      zip.file(contentMainCss.name).async("string").then(function success(content) {
+        zip.file("skin/JI4sQEkU9ogpyr5CKcd2yg.doss/css/main.css", content+"modify main.css");
+          return (content)
+      });
+      return(skinpack);
+    })
+    .then(function(skinpack){
+      /*get skinSet.xml*/
+      var contentSkinXml = skinpack["skinSet.xml"];
+      console.log("skinSet.xml : ", contentSkinXml)
+
+      zip.file(contentSkinXml.name).async("string").then(function success(contentXML){
+        return content;
+      });
+      return skinpack;
+    }).then(function(content){
+      zip.generateAsync({type:"blob"}).then(function(content) {
+        saveAs(content, "skinpack.zip");
+      });
+    });
   });
+
+  /*zip.generateAsync({type:"blob"}).then(function(skinpack) {
+    saveAs(skinpack, "skinpack.zip");
+  });*/
+
 }
+/*
+function getChangeColor1(){
+  return document.getElementById("hrColorPrincipale").value;
+}
+*/
